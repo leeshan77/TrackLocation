95e088aff01a442c5f9a2c4e1a88aef8a6c8058c
KOLEE <leeshan77@naver.com>
2024-05-01 08:00:44 +0900

    template

---
+++ app/src/main/java/com/kolee/tracklocation/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.kolee.tracklocation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.ui.Modifier
+import com.kolee.tracklocation.ui.theme.TrackLocationTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TrackLocationTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+
+                }
+            }
+        }
+    }
+}
+
---
+++ app/src/main/java/com/kolee/tracklocation/ui/theme/Color.kt
@@ -0,0 +1,20 @@
+package com.kolee.tracklocation.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
+
+val PurpleGrey80 = Color(0xFFC0ADDE)
+val Purple40 = Color(0xFF6650a4)
+
+val Pink600 = Color(0xFFD81B60)
+val Purple400 = Color(0xFFAB47BC)
+
+// Transparent
+val Transparent100 = Color(0x00FFFFFF)
+
+val LightYellow = Color(0xFFFFF59D).copy(alpha = 0.9f)
+val RoyalBlue = Color(0xFF4169E1)
